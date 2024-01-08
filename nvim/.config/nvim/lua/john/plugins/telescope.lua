20c906b59e8d96c3f6d5438b87dcc7b5284cf837
John McGuin <john@waysoftware.dev>
2024-01-08 15:25:08 -0700

    feat: grep current word under cursor

--- nvim/.config/nvim/lua/john/plugins/telescope.lua
+++ nvim/.config/nvim/lua/john/plugins/telescope.lua
@@ -32,6 +32,7 @@ return {
       vim.keymap.set('n', '<leader>p', builtin.find_files, { desc = "[F]ind [F]iles"})
       vim.keymap.set('n', '<leader>fg', builtin.git_files, { desc = "[F]ind [G]it files"})
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = "[S]earch [G]rep"})
+      vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = "[S]earch Grep current [W]ord"})
       vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "[F]ind [B]uffers"})
       vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "[F]ind [H]elp tags"})
       vim.keymap.set('n', '<leader>fj', builtin.jumplist, { desc = "[F]ind [J]umplist"})
