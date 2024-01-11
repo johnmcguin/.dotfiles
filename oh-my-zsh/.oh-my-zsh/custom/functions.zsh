66b200599c3dcec27885d80ddb467fc2953779bc
John McGuin <john@waysoftware.dev>
2024-01-11 09:16:54 -0700

    feat: time travel navigation helper. select from recent history

--- oh-my-zsh/.oh-my-zsh/custom/functions.zsh
+++ oh-my-zsh/.oh-my-zsh/custom/functions.zsh
@@ -19,3 +19,15 @@ nscripts() {
 function mkdircd() {
   mkdir -p $1 && cd $_
 }
+
+function travel() {
+    local options=()
+    # Get recent directories and create an array of local options.
+    # Can't directly `select` over $(dirs -p) due to formatting
+    while read -r dir; do
+        options+=("$dir")
+    done < <(dirs -p)
+
+    select dir in "${options[@]}"; do test -n "$dir" && break; echo "Invalid Selection"; done
+    eval cd "$dir"
+}
