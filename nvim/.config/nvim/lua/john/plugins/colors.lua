179fb13c90481d80ef984f43056fbbf019f03f44
John McGuin <john@waysoftware.dev>
2024-01-15 12:18:22 -0700

    fix: move zsh custom to allow for oh my zsh upgrades

--- nvim/.config/nvim/lua/john/plugins/colors.lua
+++ nvim/.config/nvim/lua/john/plugins/colors.lua
@@ -1,6 +1,8 @@
 return {
  	 "savq/melange-nvim" ,
  	 "folke/tokyonight.nvim",
+   "gilgigilgil/anderson.vim",
+   "morhetz/gruvbox",
    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
    { "rose-pine/neovim", name = "rose-pine" },
 }
