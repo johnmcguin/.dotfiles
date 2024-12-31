5439794b5860d26b2f6e906901e523de40ab4b09
John McGuin <john.mcguin@dat.com>
2024-12-31 08:48:21 -0700

    feat: DAT nvim config

--- oh-my-zsh-custom/.oh-my-zsh-custom/custom/alias.zsh
+++ oh-my-zsh-custom/.oh-my-zsh-custom/custom/alias.zsh
@@ -4,3 +4,7 @@ alias ..2="cd ../.."
 alias ..3="cd ../../.."
 alias ..4="cd ../../../.."
 alias python="python3"
+# nvim default command should open my DAT config
+alias nvim="NVIM_APPNAME=nvim-dat nvim"
+# pnvim should open "personal" nvim
+alias pnvim="nvim"
